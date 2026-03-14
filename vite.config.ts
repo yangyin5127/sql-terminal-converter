4a0ced53594f856cdb18668e4f84582d244b0223
Yin Yang <3375497+yangyin5127@users.noreply.github.com>
2026-03-14 15:59:53 +0800

    Revert "Add Cloudflare Workers configuration"

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss(), cloudflare()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
